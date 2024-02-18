596adc18cb6b64166614023b04ac14f185d80263
Michael Connolly <mpc63@uakron.edu>
2024-02-18 05:20:47 -0500

    Add DragWrapper component

--- classy-hud/packages/ui/src/pages/hud/Hud.tsx
+++ classy-hud/packages/ui/src/pages/hud/Hud.tsx
@@ -3,11 +3,12 @@ import { HeartIcon } from '@heroicons/react/24/solid'
 import HudSettings from './components/HudSettings'
 import { useExitListener } from '../../utils/exitListener'
 import { fetchNui } from '../../utils/nui'
+import DragWrapper from './components/DragWrapper'
 
 const Hud = () => {
   const [health, setHealth] = useState<number>(100)
   const [editMode, setEditMode] = useState<boolean>(false)
-  const [settings, setSettings] = useState<boolean>(false)
+  const [settings, setSettings] = useState<boolean>(true)
 
   useEffect(() => {
     if (editMode) {
@@ -46,94 +47,6 @@ const Hud = () => {
     await fetchNui('exitSettings')
   }
 
-  // TODO: change the drag and drop logic specifically how to add / remove from elements
-  function dragElement(elmnt: HTMLElement) {
-    var pos1 = 0,
-      pos2 = 0,
-      pos3 = 0,
-      pos4 = 0
-    if (document.getElementById(elmnt.id)) {
-      const element = document.getElementById(elmnt.id)
-      if (element) {
-        element.onmousedown = dragMouseDown
-      }
-    } else {
-      elmnt.onmousedown = dragMouseDown
-    }
-
-    function dragMouseDown(e: MouseEvent) {
-      e.preventDefault()
-
-      pos3 = e.clientX
-      pos4 = e.clientY
-
-      document.onmouseup = closeDragElement
-      document.onmousemove = elementDrag
-    }
-
-    function elementDrag(e: MouseEvent) {
-      e.preventDefault()
-
-      pos1 = pos3 - e.clientX
-      pos2 = pos4 - e.clientY
-      pos3 = e.clientX
-      pos4 = e.clientY
-
-      if (elmnt.offsetTop - pos2 < 0) {
-        elmnt.style.top = 0 + 'px'
-      } else if (elmnt.offsetTop - pos2 > window.innerHeight - elmnt.offsetHeight) {
-        elmnt.style.top = window.innerHeight - elmnt.offsetHeight + 'px'
-      } else {
-        elmnt.style.top = elmnt.offsetTop - pos2 + 'px'
-      }
-      if (elmnt.offsetLeft - pos1 < 0) {
-        elmnt.style.left = 0 + 'px'
-      } else if (elmnt.offsetLeft - pos1 > window.innerWidth - elmnt.offsetWidth) {
-        elmnt.style.left = window.innerWidth - elmnt.offsetWidth + 'px'
-      } else {
-        elmnt.style.left = elmnt.offsetLeft - pos1 + 'px'
-      }
-    }
-
-    function closeDragElement() {
-      document.onmouseup = null
-      document.onmousemove = null
-    }
-  }
-
-  // Improve this
-  if (editMode) {
-    const drag = document.getElementById('stats')
-    if (drag) {
-      dragElement(drag)
-    }
-
-    const element = document.getElementById('health')
-    if (element) {
-      dragElement(element)
-    }
-
-    const element2 = document.getElementById('health-circle')
-    if (element2) {
-      dragElement(element2)
-    }
-  } else {
-    const drag = document.getElementById('stats')
-    if (drag) {
-      drag.onmousedown = null
-    }
-
-    const element = document.getElementById('health')
-    if (element) {
-      element.onmousedown = null
-    }
-
-    const element2 = document.getElementById('health-circle')
-    if (element2) {
-      element2.onmousedown = null
-    }
-  }
-
   return (
     <main
       className={`h-screen w-screen absolute ${
@@ -148,54 +61,34 @@ const Hud = () => {
           <p className='font-bold text-5xl select-none'>Edit Mode</p>
         </div>
       ) : null}
-      <section
-        id='stats'
-        className={`${
-          editMode ? 'bg-blue-400/50 border-2 border-dashed border-blue-700 cursor-grab' : ''
-        } select-none absolute grid place-items-center text-center`}
-      >
-        <p className={`${editMode ? '' : 'hidden'} absolute text-blue-700 font-bold`}>Stats</p>
-        <div className={`flex gap-2 ${editMode ? 'invisible' : ''}`}>
-          <div className='flex items-center justify-center gap-2'>
-            <HeartIcon className='w-6 h-6' />
-            <p>{health}%</p>
-          </div>
-          <div className='flex items-center justify-center gap-2'>
-            <HeartIcon className='w-6 h-6' />
-            <p>{health}%</p>
-          </div>
+      <DragWrapper id='stats' editMode={editMode}>
+        <div className='flex items-center justify-center gap-2'>
+          <HeartIcon className='w-6 h-6' />
+          <p>{health}%</p>
         </div>
-      </section>
-      <section
-        id='health'
-        className={`${
-          editMode ? 'bg-blue-400/50 border-2 border-dashed border-blue-700 cursor-grab' : ''
-        } select-none absolute grid place-items-center text-center`}
-      >
-        <p className={`${editMode ? '' : 'hidden'} absolute text-blue-700 font-bold`}>Stats</p>
-        <div className={`flex gap-2 ${editMode ? 'invisible' : ''}`}>
-          <div className='w-96 h-10 bg-green-400/50'>
-            <div style={{ width: `${health}%` }} className='bg-green-400 h-full'></div>
-          </div>
+        <div className='flex items-center justify-center gap-2'>
+          <HeartIcon className='w-6 h-6' />
+          <p>{health}%</p>
         </div>
-      </section>
-      <section
-        id='health-circle'
-        className={`${
-          editMode ? 'bg-blue-400/50 border-2 border-dashed border-blue-700 cursor-grab' : ''
-        } select-none absolute grid place-items-center text-center`}
-      >
-        <p className={`${editMode ? '' : 'hidden'} absolute text-blue-700 font-bold`}>Stats</p>
-        <div className={`flex gap-2 ${editMode ? 'invisible' : ''}`}>
-          <div className='w-10 h-10 rounded-full overflow-hidden bg-green-400/50 flex place-items-center justify-center'>
-            <HeartIcon className='w-6 h-6 absolute' />
-            <div
-              style={{ height: `${health}%` }}
-              className='bg-green-400 w-full place-self-end'
-            ></div>
-          </div>
+      </DragWrapper>
+      <DragWrapper id='health-bar' editMode={editMode}>
+        <div className='w-96 h-10 bg-green-400/50'>
+          <div style={{ width: `${health}%` }} className='bg-green-400 h-full'></div>
         </div>
-      </section>
+      </DragWrapper>
+      <DragWrapper id='health circle' editMode={editMode}>
+        <div className='w-10 h-10 rounded-full overflow-hidden bg-green-400/50 flex place-items-center justify-center'>
+          <HeartIcon className='w-6 h-6 absolute' />
+          <div
+            style={{ height: `${health}%` }}
+            className='bg-green-400 w-full place-self-end'
+          ></div>
+        </div>
+      </DragWrapper>
+      <DragWrapper editMode={editMode} id='test'>
+        <div>test</div>
+        <div>test</div>
+      </DragWrapper>
     </main>
   )
 }
---
+++ classy-hud/packages/ui/src/pages/hud/components/DragWrapper.tsx
@@ -0,0 +1,87 @@
+interface DragWrapperProps {
+  children: JSX.Element | JSX.Element[]
+  editMode: boolean
+  id: string
+}
+
+// TODO: change the drag and drop logic
+function dragElement(elmnt: HTMLElement) {
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0
+  if (document.getElementById(elmnt.id)) {
+    const element = document.getElementById(elmnt.id)
+    if (element) {
+      element.onmousedown = dragMouseDown
+    }
+  } else {
+    elmnt.onmousedown = dragMouseDown
+  }
+
+  function dragMouseDown(e: MouseEvent) {
+    e.preventDefault()
+
+    pos3 = e.clientX
+    pos4 = e.clientY
+
+    document.onmouseup = closeDragElement
+    document.onmousemove = elementDrag
+  }
+
+  function elementDrag(e: MouseEvent) {
+    e.preventDefault()
+
+    pos1 = pos3 - e.clientX
+    pos2 = pos4 - e.clientY
+    pos3 = e.clientX
+    pos4 = e.clientY
+
+    if (elmnt.offsetTop - pos2 < 0) {
+      elmnt.style.top = 0 + 'px'
+    } else if (elmnt.offsetTop - pos2 > window.innerHeight - elmnt.offsetHeight) {
+      elmnt.style.top = window.innerHeight - elmnt.offsetHeight + 'px'
+    } else {
+      elmnt.style.top = elmnt.offsetTop - pos2 + 'px'
+    }
+    if (elmnt.offsetLeft - pos1 < 0) {
+      elmnt.style.left = 0 + 'px'
+    } else if (elmnt.offsetLeft - pos1 > window.innerWidth - elmnt.offsetWidth) {
+      elmnt.style.left = window.innerWidth - elmnt.offsetWidth + 'px'
+    } else {
+      elmnt.style.left = elmnt.offsetLeft - pos1 + 'px'
+    }
+  }
+
+  function closeDragElement() {
+    document.onmouseup = null
+    document.onmousemove = null
+  }
+}
+
+const DragWrapper = ({ children, editMode, id }: DragWrapperProps) => {
+  const element = document.getElementById(id)
+
+  if (element) {
+    if (editMode) dragElement(element)
+    else element.onmousedown = null
+  }
+
+  return (
+    <div
+      id={id}
+      className={`${
+        editMode ? 'bg-blue-400/50 border-2 border-dashed border-blue-700 cursor-grab' : ''
+      } select-none absolute grid place-items-center text-center`}
+    >
+      <p
+        className={`${editMode ? '' : 'hidden'} absolute text-blue-400 font-bold whitespace-nowrap`}
+      >
+        {id}
+      </p>
+      <div className={`flex gap-2 ${editMode ? 'invisible' : ''}`}>{children}</div>
+    </div>
+  )
+}
+
+export default DragWrapper
